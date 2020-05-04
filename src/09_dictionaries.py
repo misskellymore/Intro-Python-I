63f119dd2c3202d1a15268f3748a33e98e4286d7
moreira.kd@outlook.com <moreira.kd@outlook.com>
2020-05-04 11:10:28 -0400

    11

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -54,6 +54,7 @@ print(waypoints)
 waypoints[0]["lon"] = -130
 waypoints[0]["name"] = "not a real place"
 print(waypoints)
+
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
 for n in waypoints:
